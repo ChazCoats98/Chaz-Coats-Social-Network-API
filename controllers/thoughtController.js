14bdf820a630fe7eeee94c0ca5cf39b8c7452937
Chaz Coats <chazcoats2@hotmail.com>
2023-10-10 11:17:29 -0400

    working on thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,5 @@
 const Thought = require('../models/thought');
+const user = require('../models/user');
 
 module.exports = {
     async getThoughts(req, res) {
@@ -8,5 +9,28 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
-    }
+    },
+    async getOneThought(req, res) {
+        try {
+            const oneThought = await Thought.findOne({_id: req.params.id});
+            res.json(oneThought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async newThought(req, res) {
+        try{
+            const thoughtData = await Thought.create(req.body);
+            res.json(thoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateThought(req, res) {
+        try {
+            const updateThought = await Thought.FindOneAndUpdate({_id: req.params.id}, {thoughtText: req.body})
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 }
