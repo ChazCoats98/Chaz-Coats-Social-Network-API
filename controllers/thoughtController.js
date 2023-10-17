452fbe073d047440ce8cbc0f8802f311064eaed7
Chaz Coats <chazcoats2@hotmail.com>
2023-10-17 11:16:28 -0400

    cant delete thoughts?

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,6 +2,7 @@ const Thought = require('../models/thought');
 User = require('../models/user');
 
 module.exports = {
+    //gets all thoughts
     async getThoughts(req, res) {
         try {
             const thoughts = await Thought.find().populate('reactions');
@@ -10,6 +11,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    //gets thought by id
     async getOneThought(req, res) {
         try {
             const oneThought = await Thought.findOne({_id: req.params.thoughtId}).populate('reactions');
@@ -18,6 +20,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    //creates thought and pushes it to thoughts array in user schema
     async newThought(req, res) {
         try{
             const thoughtData = await Thought.create(req.body);
@@ -28,6 +31,7 @@ module.exports = {
             console.log(err);
         }
     },
+    //updates thought 
     async updateThought(req, res) {
         try {
             const updateThoughtData = await Thought.findOneAndUpdate({_id: req.params.thoughtId}, {$set: req.body}, {runValidators: true, new: true});
@@ -36,14 +40,16 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    //deletes thought 
     async deleteThought(req, res) {
         try {
             const thoughtEraser = await Thought.findOneAndDelete({_id: req.params.thoughtId})
-            res.json(thoughtEraser);
+            res.json({ message: "Thought deleted"});
         } catch (err) {
             res.status(500).json(err);
         }
     },
+    //creates reaction and pushes it to reaction array in thought schema 
     async createReaction(req, res) {
         try {
             const reactionBuilder = await Thought.findOneAndUpdate({_id: req.params.thoughtId}, { $push: { reactions: req.body}}, {runValidators: true, new: true});
@@ -52,6 +58,7 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    //deletes reaction 
     async deleteReaction(req, res) {
         try {
             const reactionEraser = await reaction.findOneAndDelete({_id: req.params.thoughtId}, { $pull: { reactions: { reactionId: req.params.reactionId}}}, { new: true });
