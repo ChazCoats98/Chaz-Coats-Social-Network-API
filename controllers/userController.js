c97febcea845f2c558aba7d0c95c6bd33bf64379
Chaz Coats <chazcoats2@hotmail.com>
2023-10-17 10:38:09 -0400

    moving on from populating. adding add friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const User = require('../models/user');
 
 module.exports = {
+    //queries DB for all user data then displays in JSON format
     async getUsers(req, res) {
         try {
-            const users = await User.find();
+            const users = await User.find().populate('thoughts').populate('reactions');
             res.json(users);
         } catch (err) {
             res.status(500).json(err);
@@ -11,8 +12,9 @@ module.exports = {
         }
     },
     async getOneUser(req, res) {
+        //cant get thoughts or reactions to populate.
         try {
-            const user = await User.findOne({ _id: req.params.userId }).select('-__v').populate('thoughts').populate('friends');
+            const user = await User.findOne({ _id: req.params.userId }).populate('thoughts').populate('reactions');
             res.json(user);
         } catch (err) {
             res.status(500).json(err);
@@ -27,6 +29,14 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    async updateUser(req, res) {
+        try {
+            const updatedUser = await User.findOneAndUpdate({ _id: req.params.userId}, { $set: req.body }, { runValidators: true, new: true});
+            res.json(updatedUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
     async deleteUser(req, res) {
         try {
             const userEraser = await User.FindOneAndDelete({ _id: req.params.userId });
@@ -34,5 +44,10 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err);
         }
+    },
+    async addFriend(req, res) {
+        try {
+            const newFriend = await User.findOneAndUpdate({ _id: req.params.userId}, { $addToSet: })
+        }
     }
-}
+} 
